8c0f49f9ca715efcddbbb59e343f18fe162cc0f7
Danil Olesik <danil0077@bk.ru>
2024-12-27 16:37:16 +1000

    Get random qoutes via public API service

--- src/classes/Quote.js
+++ src/classes/Quote.js
@@ -16,4 +16,26 @@ class Quote {
 
 new Quote();
 
+// // URL на сырой файл JSON
+// const url =
+//   "https://raw.githubusercontent.com/JamesFT/Database-Quotes-JSON/master/quotes.json";
+
+// // Получение случайной цитаты
+// fetch(url)
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error(`Ошибка HTTP: ${response.status}`);
+//     }
+//     return response.json();
+//   })
+//   .then((data) => {
+//     // Получение случайной цитаты
+//     const randomIndex = Math.floor(Math.random() * data.length);
+//     const randomQuote = data[randomIndex];
+
+//     // Вывод случайной цитаты
+//     console.log(`"${randomQuote.quoteText}" — ${randomQuote.quoteAuthor}`);
+//   })
+//   .catch((error) => console.error(`Ошибка загрузки данных: ${error.message}`));
+
 export default Quote;
