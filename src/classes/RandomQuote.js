f5bea89d86342ac0e3c8db49d9750ce5cc32e054
Danil Olesik <danil0077@bk.ru>
2025-01-27 14:41:30 +1000

    Removing comments for the async/await

--- src/classes/RandomQuote.js
+++ src/classes/RandomQuote.js
@@ -9,25 +9,6 @@ class RandomQuote {
     return new Quote(id, text, author);
   }
 
-  /**
-   * 1. Each async function returns Promise
-   * 2. If on the "await" line of code Promise is rejected, code in the same block
-   * below "await" is not executed"
-   * 3. Promise returned by the getEandomQuoteViaAPI function will be always "fulfilled"
-   * because we catch all possible errors
-   * 4. Result of the fulfilled promise will be either Quote or underfined
-   * 5. Therefore there is no need for try/catch block where we call this function
-   */
-
-  /**
-   * 1. Любая асинхронная функция возвращает промис
-   * 2. ЕСли на строке с "await" промис отклоняется, то код ниже в этом же блоке
-   * не выполняется
-   * 3. Промис, который возвращает функция getEandomQuoteViaAPI всегда будет "fulfilled",
-   * потому что есть try/catch блок
-   * 4. Результатом fulfilled промиса будет  Quote или underfined
-   * 5. Следовательно нет необходимости использовать try/catch, где мы вызываем эту функцию
-   */
   static async getRandomQuoteViaAPI() {
     const url = "https://api.agify.io/?name=meelad";
     const options = { headers: { "Content-Type": "application/json" } };
@@ -38,25 +19,8 @@ class RandomQuote {
       return new Quote(id, content, author);
     } catch (error) {
       console.error(error);
-      /**
-       * 1. Returns undefined implicitly (resolved promise to undefined)
-       * 2. Promise becomes "fulfilled"
-       */
     }
   }
-
-  // static getRandomQuoteViaAPI() {
-  //   const url = "https://api.agify.io/?name=meelad";
-  //   const options = { headers: { "Content-Type": "application/json" } };
-
-  //   return fetch(url, options)
-  //     .then((response) => response.json())
-  //     .then(
-  //       ({ count: id, name: content, age: author }) =>
-  //         new Quote(id, content, author)
-  //     )
-  //     .catch((error) => console.error(error));
-  // }
 }
 
 export default RandomQuote;
